da97b4628c0a68d4e199ac96d6b761784a2c2871
FEM3 <franklinemcgowaniii@gmail.com>
2026-04-18 22:22:33 -0600

    Add files via upload

---
+++ CSC450_CT1_fictperson.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+    string firstName = "Frank";
+    string lastName = "McGowan";
+    string streetAddress = "1234 1st Street";
+    string cityName = "Denver";
+    string zipCode = "80219";
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Street Address: " << streetAddress << endl;
+    cout << "City: " << cityName << endl;
+    cout << "Zip Code: " << zipCode << endl;
+
+    cin.get();
+
+    return 0;
+}
